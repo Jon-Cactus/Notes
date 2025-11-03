a599cf7188f5b7553dd5a96152e3ef6c8b656f7c
Jon-Cactus <jonathantcspence@gmail.com>
2025-11-03 22:15:51 +0900

    successfully toggle the hidden class for replies when clicking the reply button

--- Scrimba/EssentialJavaScript/BuildAnXClone/index.js
+++ Scrimba/EssentialJavaScript/BuildAnXClone/index.js
@@ -13,8 +13,11 @@ document.addEventListener('click', function(e){
     else if(e.target.dataset.retweet){
         handleRetweetClick(e.target.dataset.retweet)
     }
+    else if(e.target.dataset.reply){
+        handleReplyClick(e.target.dataset.reply)
+    }
 })
-
+ 
 function handleLikeClick(tweetId){ 
     const targetTweetObj = tweetsData.filter(function(tweet){
         return tweet.uuid === tweetId
@@ -45,6 +48,19 @@ function handleRetweetClick(tweetId){
     render() 
 }
 
+function handleReplyClick(replyId){
+    const targetReply = document.getElementById(`replies-${replyId}`)
+    targetReply.classList.toggle('hidden')
+/*
+Challenge:
+1. Use the uuid stored in 'replyId' to take control 
+   of the div containing that tweet’s replies. 
+   (Check the HTML string below to remind yourself 
+   what id that div will have.)  
+2. Toggle the CSS class "hidden" on that div. 
+*/ 
+}
+
 function getFeedHtml(){
     let feedHtml = ``
     
@@ -65,36 +81,19 @@ function getFeedHtml(){
         let repliesHtml = ''
         
         if(tweet.replies.length > 0){
-            tweet.replies.forEach((reply) => {
-                repliesHtml += `
-                    <div class="tweet-reply">
-                        <div class="tweet-inner">
-                            <img src="${reply.profilePic}" class="profile-pic">
-                            <div>
-                                <p class="handle">${reply.handle}</p>
-                                <p class="tweet-text">${reply.tweetText}</p>
-                            </div>
-                        </div>
-                    </div>`
-            })
-            
-/*
-Challenge:
-1. If a tweet has replies, iterate through the replies
-   and wrap each one in the HTML template provided below. 
-   Make sure to replace words in UPPERCASE with data from 
-   the tweet. On each iteration, add this HTML to repliesHtml.
-   
+            tweet.replies.forEach(function(reply){
+                repliesHtml+=`
 <div class="tweet-reply">
     <div class="tweet-inner">
-        <img src="PROFILE PIC" class="profile-pic">
+        <img src="${reply.profilePic}" class="profile-pic">
             <div>
-                <p class="handle">HANDLE</p>
-                <p class="tweet-text">TWEET TEXT</p>
+                <p class="handle">${reply.handle}</p>
+                <p class="tweet-text">${reply.tweetText}</p>
             </div>
         </div>
 </div>
-*/
+`
+            })
         }
         
           
@@ -127,18 +126,11 @@ Challenge:
             </div>   
         </div>            
     </div>
-    <div id="replies-${tweet.uuid}">
+    <div class="hidden" id="replies-${tweet.uuid}">
         ${repliesHtml}
     </div>   
 </div>
 `
-/*
-Challenge:
-2. Place repliesHtml in its parent div remembering 
-   to update that divs id.
-*/
-
-
    })
    return feedHtml 
 }
