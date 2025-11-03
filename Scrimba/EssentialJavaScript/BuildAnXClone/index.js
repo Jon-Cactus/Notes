1cf2ebb1f718646112fe895a5c893a937f70596c
Jon-Cactus <jonathantcspence@gmail.com>
2025-11-03 14:36:45 +0900

    successfully conditionally add classes to the like and retweet button depending upon boolean status

--- Scrimba/EssentialJavaScript/BuildAnXClone/index.js
+++ Scrimba/EssentialJavaScript/BuildAnXClone/index.js
@@ -8,18 +8,11 @@ tweetBtn.addEventListener('click', function(){
 
 document.addEventListener('click', function(e){
     if(e.target.dataset.like){
-        handleLikeClick(e.target.dataset.like) 
+       handleLikeClick(e.target.dataset.like) 
     }
-    
-    else if(e.target.dataset.retweet) {
+    else if(e.target.dataset.retweet){
         handleRetweetClick(e.target.dataset.retweet)
     }
-/*
-Challenge:
-1. Make this eventListener call "handleRetweetClick" 
-   when the retweet icon is clicked, passing in the
-   uuid from that tweet.  
-*/
 })
 
 function handleLikeClick(tweetId){ 
@@ -38,31 +31,46 @@ function handleLikeClick(tweetId){
 }
 
 function handleRetweetClick(tweetId){
-    const targetTweetObj = tweetsData.filter(function(tweet) {
+    const targetTweetObj = tweetsData.filter(function(tweet){
         return tweet.uuid === tweetId
     })[0]
     
-    if (targetTweetObj.isRetweeted) {
+    if(targetTweetObj.isRetweeted){
         targetTweetObj.retweets--
-    } else {
+    }
+    else{
         targetTweetObj.retweets++
     }
     targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
-    render()
-/*
-Challenge:
-2. Find the retweeted tweet's object in tweetsData 
-   and save it to a const.
-3. Increment or decrement the retweet count of the 
-   tweet and flip its isRetweeted boolean.
-4. Call the render function.  
-*/   
+    render() 
 }
 
 function getFeedHtml(){
     let feedHtml = ``
     
     tweetsData.forEach(function(tweet){
+        
+        let likeIconClass = ''
+        
+        if (tweet.isLiked){
+            likeIconClass = 'liked'
+        }
+        
+        let retweetIconClass = ''
+        
+        if (tweet.isRetweeted) {
+            retweetIconClass = 'retweeted'
+        }
+        
+/*
+Challenge:
+1. Use an if statement to set the value of 
+   'retweetIconClass' to the string 
+   'retweeted' if the tweet has been retweeted. 
+2. In the retweet icon tag, add 'retweetIconClass' 
+   to the list of classes.
+*/
+          
         feedHtml += `
 <div class="tweet">
     <div class="tweet-inner">
@@ -78,13 +86,13 @@ function getFeedHtml(){
                     ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
-                    <i class="fa-solid fa-heart"
+                    <i class="fa-solid fa-heart ${likeIconClass}"
                     data-like="${tweet.uuid}"
                     ></i>
                     ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
-                    <i class="fa-solid fa-retweet"
+                    <i class="fa-solid fa-retweet ${retweetIconClass}"
                     data-retweet="${tweet.uuid}"
                     ></i>
                     ${tweet.retweets}
