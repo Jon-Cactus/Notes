e327cd5161785812f8f04b5153564d8dbf312626
Jon-Cactus <jonathantcspence@gmail.com>
2025-11-03 10:16:59 +0900

    took control of feed div and inserted HTML for the feed

--- Scrimba/EssentialJavaScript/BuildAnXClone/index.js
+++ Scrimba/EssentialJavaScript/BuildAnXClone/index.js
@@ -8,13 +8,9 @@ tweetBtn.addEventListener('click', function(){
 
 function getFeedHtml(){
     
-/*
-Challenge:
-1. Replace the for of with a forEach.
-*/
-      
     let feedHtml = ``
-    tweetsData.forEach((tweet) => {
+    
+    tweetsData.forEach(function(tweet){
         feedHtml += `
 <div class="tweet">
     <div class="tweet-inner">
@@ -41,4 +37,16 @@ Challenge:
    return feedHtml 
 }
 
-console.log(getFeedHtml())
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
+/*
+Challenge:
+1. Take control of the ‘feed’ div.
+2. Render the HTML returned by the getFeedHtml 
+   function to the 'feed' div. 
+   See if you can do this with just one line of code!
+*/
+}
+render()
+// call render
+
