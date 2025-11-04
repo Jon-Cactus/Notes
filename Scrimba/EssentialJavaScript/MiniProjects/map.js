88859a6c08d990bfb405bb82722db0f542bb1326
Jon-Cactus <jonathantcspence@gmail.com>
2025-11-04 22:14:48 +0900

    completed mini challenge

---
+++ Scrimba/EssentialJavaScript/MiniProjects/map.js
@@ -0,0 +1,27 @@
+import { playlistArr } from '/playlist.js'
+
+/*
+Challenge
+1. Refactor the code below to use .map() 
+   instead of the for loop.
+   ⚠️ Don't worry about the commas for now.
+*/
+
+const playlistHtml = playlistArr.map(function(song, index) {
+   return `
+<section class="card">
+    <div class="card-start">
+        <img src="/images/${song.albumArt}">
+    </div>
+        <div class="card-mid">
+            <h4 class="card-title">${song.title}</h4>
+            <p class="card-artist">${song.artist}</p>
+        </div>
+    <div class="card-end">
+        <p class="card-menu">...</p>
+    </div>
+</section>
+    `
+})
+
+document.getElementById('container').innerHTML = playlistHtml
